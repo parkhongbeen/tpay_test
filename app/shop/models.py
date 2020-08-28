074299f38208fa2cfcbc125933860fb3c68df7fa
parkhongbeen <pack122@naver.com>
2020-08-28 18:36:08 +0900

    feat: modeling완료, urls.py생성,연결

---
+++ app/shop/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField('상품명', max_length=100)
+    tag = models.ManyToManyField('Tag', max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductOption(models.Model):
+    option = models.CharField('상품 옵션', max_length=100)
+    price = models.PositiveIntegerField('가격')
+    product = models.ForeignKey(Product, verbose_name='상품',
+                                on_delete=models.CASCADE,
+                                related_name='option')
+
+    def __str__(self):
+        return self.option
+
+
+class Tag(models.Model):
+    name = models.CharField('태그명', max_length=100)
+
+    def __str__(self):
+        return self.name
