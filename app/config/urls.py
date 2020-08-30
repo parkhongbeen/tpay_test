496f90bed2d5eea743eedb82d9b5039188a1ac27
parkhongbeen <pack122@naver.com>
2020-08-31 05:20:40 +0900

    fix: yasg설정 수정

--- app/config/urls.py
+++ app/config/urls.py
@@ -21,18 +21,17 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="Tpay_backend_API",
         default_version='v1',
-        description="TpayTest description",
+        description="**API for Tpay backend**",
+        contact=openapi.Contact(email='phb@tpay.com')
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=(permissions.AllowAny,)
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^doc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
+    path('doc/', schema_view.with_ui('redoc', cache_timeout=0))
 ]
